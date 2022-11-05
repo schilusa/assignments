d5a3946231a050dc19f8de9189ee9b7afe67407e
schilusa <115701071+schilusa@users.noreply.github.com>
2022-11-05 23:16:32 +0530

    String2

---
+++ String2.java
@@ -0,0 +1,11 @@
+
+package com.company;
+import java.util.*;
+public class String2 {
+    public static void main(String[] args) {
+        String a="Hello";
+        String b=" how are you?";
+        String c=a.concat(b);
+        System.out.println(c);
+    }
+}

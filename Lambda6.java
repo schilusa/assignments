c71e46cd3ecd2f459609c13a786abb00963d7f2e
schilusa <115701071+schilusa@users.noreply.github.com>
2022-11-05 14:49:11 +0530

    Lambda6

---
+++ Lambda6.java
@@ -0,0 +1,21 @@
+package com.lambda;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+public class Lambda6 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+        Lambda6 Letter = new Lambda6();
+        System.out.println(Letter.convertToUpperCase(list));
+    }
+
+    public List<String> convertToUpperCase(List<String> list) {
+        List<String> ToUpper = new ArrayList<>();
+        Consumer<String> consumer = (str) -> ToUpper.add(str.toUpperCase());
+        list.stream()
+                .forEach(consumer);
+        return ToUpper;
+    }
+}
+

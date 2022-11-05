81bd2c2d77eb8cfd4dffd1f24849ae3b20ba791d
schilusa <115701071+schilusa@users.noreply.github.com>
2022-11-05 22:42:30 +0530

    Test

---
+++ Test.java
@@ -0,0 +1,30 @@
+package com.Oop;
+abstract class Persistance {
+    abstract void persist();
+}
+class Filepersistence extends Persistance{
+    void persist()
+    {
+        System.out.println("data is stored in files");
+    }
+}
+class DatabasePersistence extends Persistance{
+    void persist()
+    {
+        System.out.println("Data is stored in Database");
+    }
+}
+class Test
+{
+    public static void main(String args[])
+    {
+        Filepersistence f=new Filepersistence();
+        DatabasePersistence d=new DatabasePersistence();
+        f.persist();
+        d.persist();
+    }
+}
+
+
+
+

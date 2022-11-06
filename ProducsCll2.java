3ba76f6fef70da1fd0b9cc0b95b5422b2cd1a3eb
schilusa <115701071+schilusa@users.noreply.github.com>
2022-11-06 11:15:11 +0530

    ProducsCll2

---
+++ ProducsCll2.java
@@ -0,0 +1,22 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class ProducsCll2 {
+    public static void main(String[] args)
+    {
+        HashSet<String> obj=new HashSet<>();
+        obj.add("Cups");
+        obj.add("chairs");
+        obj.add("pens");
+        obj.add("table");
+        obj.add("Books");
+        obj.add("papers");
+        obj.add("flank");
+        obj.add("breshes");
+        obj.add("colors");
+        obj.add("mobiles");
+        System.out.println(obj);
+    }
+}
+
